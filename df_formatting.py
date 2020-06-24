dd87110e742afd1de3ed02e29d318b7a3a5ca298
Yash Karandikar <25213173+y0shK@users.noreply.github.com>
2020-06-24 11:06:34 -0700

    title & author/index keywords + varianceThreshold works

--- df_formatting.py
+++ df_formatting.py
@@ -35,12 +35,13 @@ def get_all_keywords(text, word_boolean):
     total_phrases = []
 
     for local_text in text:
-        local_keywords = r.extract_keywords_from_text(local_text) # necessary, even though local_keywords is never used
-        local_phrases = r.get_ranked_phrases_with_scores()
+        r.extract_keywords_from_text(local_text) # use the Rake instance to find the keywords
+        local_phrases = r.get_ranked_phrases_with_scores() # apply these keywords to a local variable
         total_phrases.append(local_phrases)
 
     all_keywords = []
 
+    # get_ranked_phrases_with_scores gives a list of tuples - tuple[0] is score, 1 is word
     for list in total_phrases:
         for tuple in list:
             all_keywords.append(tuple[1])
@@ -68,12 +69,12 @@ def is_nan(string):
 def contains_multiple_words(string):
     return len(string.split()) > 1 # if true, the array has more than one element, i.e. more than one word
 
-def top_n_author_keywords_or_phrases(author_keywords, word_boolean, n):
+def top_n_author_keywords_or_phrases(keyword_text, word_boolean, n):
 
     # https://www.geeksforgeeks.org/python-string-split/
 
     master_list = []
-    for line in author_keywords:
+    for line in keyword_text:
         if not is_nan(line): # if the line can be parsed
             local_element = line.split(';') # split the line based on the demarcation of ;
             master_list.append(local_element) # take each element a; b; c; d and append it [a, b, c, d]
@@ -161,24 +162,46 @@ def top_n_author_keywords_or_phrases(author_keywords, word_boolean, n):
     if word_boolean:
         del author_keywords[n:] # e.g. if n=5, then [5:] takes away all but elements 0-4
         del author_keyword_labels[n:]
+        return author_keyword_labels # we care about the labels of the proportions (i.e. the words), not the proportions
 
     elif not word_boolean:
         del author_key_phrases[n:]
         del author_key_phrase_labels[n:]
+        return author_key_phrase_labels # same as word_boolean branch - we want keywords, not proportions of keywords
 
+# title (and title subset) is the df that allows us to iterate through each paper
 title = subset_df['title']
 all_key_words = get_all_keywords(title, word_boolean=True)
 all_key_phrases = get_all_keywords(title, word_boolean=False)
 
+# also add author and index keywords as their own boolean columns
+author_keywords = subset_df['author keywords']
+index_keywords = subset_df['index keywords']
+
+# can dynamically change the top n keyword amounts
+n1 = 10
+n2 = 10
+
+top_ten_author_keywords = top_n_author_keywords_or_phrases(keyword_text=author_keywords, word_boolean=True, n=n1)
+top_ten_index_keywords = top_n_author_keywords_or_phrases(keyword_text=index_keywords, word_boolean=True, n=n2)
+top_ten_author_and_index_keywords = top_ten_author_keywords + top_ten_index_keywords
+
+max_index = 100
+total_index = 200
+
 # test a subset of ideal dataframe, with 100 words
-all_key_words_subset = all_key_words[0:500] # slice of first 100 words
-all_key_phrases_subset = all_key_phrases[0:500] # slice of first 100 phrases
-all_key_data_subset = all_key_words_subset + all_key_phrases
-subset_df_lenSubset = subset_df[0:1000] # resize the df to match our test slice
+all_key_words_subset = all_key_words[0:max_index] # slice of first 100 words
+all_key_phrases_subset = all_key_phrases[0:max_index] # slice of first 100 phrases
+
+# add together all data - title, abstract, keywords
+all_key_data_subset = all_key_words_subset + all_key_phrases_subset + top_ten_author_and_index_keywords
+
+total_index = total_index + n1 + n2 # necessary to add together len of df before assigning it in slice notation
+subset_df_lenSubset = subset_df[0:total_index] # resize the df to match our test slice, and add top 10 author/index keywords
 
 words_found = []
-# look at all titles for each word for all words
 
+# look at all titles for each word for all words
 for word in all_key_data_subset:
     for row in subset_df_lenSubset['title']:
         if word.lower() in row.lower(): # similar to Java ignoreCase(), works for our purposes
@@ -187,38 +210,27 @@ for word in all_key_data_subset:
             words_found.append(0)
 
 master_list = []
-# simple enough to programmatically change, 1000^2 -> len(data size) and 1000 -> factor of data size (have to find equal groups)
-for i in range(0, 1000000):
-    # put every batch of n words together
-    if i % 1000 == 0:
-        list = words_found[i:i+1000]
+# simple enough to programmatically change, 100^2 -> len(data size) and 100 -> factor of data size (have to find equal groups)
+for i in range(0, total_index**2):
+    if i % total_index == 0:
+        list = words_found[i:i+total_index] # i.e. list 1 is indices 0 to 100, list 2 is 101 to 200, etc.
         master_list.append(list)
 
-# check if at least one keyword match occurs in each list
-empty_list_count = 0
-empty_elements = []
-for element in master_list:
-    if 1 not in element:
-        empty_elements.append(element)
-        empty_list_count += 1
-print('Empty list count', empty_list_count)
-
-# ensure that each element in the master list has one match; else, remove them
-for empty_element in empty_elements:
-    master_list.remove(empty_element)
-
-print(master_list)
+print('Master list length', len(master_list))
+print('Data frame subset length', len(subset_df_lenSubset))
+print('Length of all data', len(all_key_data_subset))
+print('\nMaster list', master_list)
 
 pd.set_option('display.max_columns', None)
 
-for i in range(len(subset_df_lenSubset)-len(empty_elements)): # for every element in the df - the empty elements being removed
-    # the df adds a new column as pointed to by keyword, then adds a column body from word batches earlier
+# for every paper in the whole df, add a new column for EACH keyword
+# then, each column is assigned its respective boolean array (is the term of the column present or not in EACH paper?)
+for i in range(len(subset_df_lenSubset)):
     subset_df_lenSubset[all_key_data_subset[i]] = master_list[i]
 
 # add boolean columns that are not keyword columns (e.g. NIH funding)
-
 funding_details = subset_df['funding details']
-funding_details = funding_details[0:1000]
+funding_details = funding_details[0:total_index]
 funding_details_bool_array = []
 
 for detail in funding_details:
@@ -233,14 +245,19 @@ subset_df_lenSubset['paper funding boolean'] = funding_details_bool_array
 
 # add non-boolean columns (e.g. year)
 year = subset_df['year']
-subset_df_lenSubset['year published'] = year[0:1000]
+subset_df_lenSubset['year published'] = year[0:total_index]
 
 author_number = subset_df['author number']
-subset_df_lenSubset['total author number'] = author_number[0:1000]
+subset_df_lenSubset['total author number'] = author_number[0:total_index]
 
 page_count = subset_df_lenSubset['paper length in pages']
-subset_df_lenSubset['page count'] = page_count[0:1000]
+subset_df_lenSubset['page count'] = page_count[0:total_index]
 
+# author and index keywords, as well as title and abstract, are above - source title can simply be appended
+source_title = subset_df_lenSubset['source title']
+subset_df_lenSubset['title of source'] = source_title[0:total_index]
+
+# data frame
 print(subset_df_lenSubset)
 
 # test out variance threshold
@@ -249,34 +266,33 @@ import numpy as np
 
 selector = VarianceThreshold(.99 * (1 - .99)) # 99% non-zero
 
-# create a fork of the variable to experiment with non-zero variance
-
 # https://stackoverflow.com/questions/34923728/type-error-unhashable-type-list-while-selecting-subset-from-specific-columns
 # need to coerce the list to tuple so that pandas can hash the subset
 # https://stackoverflow.com/questions/19371358/python-typeerror-unhashable-type-list
 
-dfmedswide = subset_df_lenSubset
-dfmedswide.drop(dfmedswide.columns.difference(tuple(all_key_data_subset)), 1, inplace=True)
+# create a fork of the variable to experiment with non-zero variance
+wide = subset_df_lenSubset
+wide.drop(wide.columns.difference(tuple(all_key_data_subset)), 1, inplace=True)
 
-selector.fit_transform(np.array(dfmedswide)).shape
+selector.fit_transform(np.array(wide)).shape
 
 def variance_threshold_selector(data, threshold=0.5):
     selector = VarianceThreshold(threshold)
     selector.fit(data)
     return data[data.columns[selector.get_support(indices=True)]]
 
-dfmedsfiltered_99_percent = variance_threshold_selector(dfmedswide, .99 * (1 - .99))
-dfmedsfiltered_97_percent = variance_threshold_selector(dfmedswide, .97 * (1 - .97))
-dfmedsfiltered_95_percent = variance_threshold_selector(dfmedswide, .95 * (1 - .95))
+dfmedsfiltered_99_percent = variance_threshold_selector(wide, .99 * (1 - .99))
+dfmedsfiltered_97_percent = variance_threshold_selector(wide, .97 * (1 - .97))
+dfmedsfiltered_95_percent = variance_threshold_selector(wide, .95 * (1 - .95))
 
 # For setting threshold for Boolean variables:
 # if you want to remove variables which are 0 or 1 for more than 99% of observations,
 # threshold=0.99*(1-0.99) assuming it’s a Bernoulli random variable
 
-# without filter: ~810 keyword columns
-# with 99% filter: ~80
-# with 97% filter: 22
-# with 95% filter: 7
+# without filter: 215 keyword columns
+# with 99% filter: 47
+# with 97% filter: 19
+# with 95% filter: 8
 print(dfmedsfiltered_99_percent)
 print(dfmedsfiltered_97_percent)
 print(dfmedsfiltered_95_percent)
