9460a69f17011d27b9f92cbdcda13974f608fea5
Yash Karandikar <25213173+y0shK@users.noreply.github.com>
2020-06-30 18:05:19 -0700

    add multiple linear regression, add summary()

--- regressionanalysis/multiple_regression.R
+++ regressionanalysis/multiple_regression.R
@@ -13,10 +13,22 @@ num_index_keywords <- all_scopus_droppedNaN$number.of.index.keywords
 # dependent variable - times cited
 times_cited <- all_scopus_droppedNaN$cited.by
 
+multiple_linReg_fit <- lm(times_cited ~ author_number + num_author_keywords + num_index_keywords,
+                          data = all_scopus_droppedNaN)
+
+summary(multiple_linReg_fit)
+
+plot(multiple_linReg_fit)
+abline(multiple_linReg_fit, col='blue')
+rsq_multiple_linReg <- summary(multiple_linReg_fit)$r.squared
+rsq_multiple_linReg # 0.0062
+
 # skip c - c is a reserved constant in R
 nonlinReg <- nls(times_cited ~ author_number*a +
                  num_author_keywords*b + num_index_keywords*d, 
                  data = all_scopus_droppedNaN)
+
+summary(nonlinReg)
 
 all_predictor_variables <- author_number + num_author_keywords + num_index_keywords
 
