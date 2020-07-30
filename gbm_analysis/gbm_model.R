6417a26349e5b0d9790635e5c794004e0f439a5c
Yash Karandikar <25213173+y0shK@users.noreply.github.com>
2020-07-30 12:22:23 -0700

    Update gbm_model.R

--- gbm_analysis/gbm_model.R
+++ gbm_analysis/gbm_model.R
@@ -1,5 +1,5 @@
-setwd('C:\\Users\\ynkar\\Desktop\\ophthalmology\\ophthalmologycitations-master')
-df_scopus_csv <- read.csv('dfscopus-2.csv')
+setwd('C:\\Users\\veena\\Desktop\\ophthalmologycitations-master\\ophthalmologycitations-master')
+df_scopus_csv <- read.csv('dfscopus-2_with_nlp_title_and_abstract.csv')
 
 # df cleanup from rmd file
 library(tidyverse)
@@ -31,9 +31,16 @@ LogLossBinary = function(actual, predicted, eps = 1e-15) {
 library(caret)
 library(e1071)
 library(pROC)
+library(precrec)
 
 # define number of trees - keep consistent with train and dev/test
-num_trees_var <- 150
+num_trees_var <- 300
+
+# create a tuneGrid to provide vectors to caret trainControl()
+gbmGrid <- expand.grid(interaction.depth = c(1, 2, 3, 4, 5, 6, 7, 8),
+                        n.trees = seq(50, 500, 50),
+                        shrinkage = seq(.001, .1, .01),
+                        n.minobsinnode = c(10))
 
 # train with train set
 # use caret library to create trmodel, then caret train() function
@@ -44,27 +51,28 @@ fit_control <- caret::trainControl(## 10-fold cv
   selectionFunction = 'best',
   savePredictions = TRUE)
 
-set.seed(1) # set seed for consistency
-caret_gbm_fit <- caret::train(as.factor(tcB) ~ . -tc -split -pagecount,
-                              data = df_train,
-                              method = 'gbm',
-                              distribution = 'bernoulli',
-                              trControl = fit_control,
+#set.seed(1) # set seed for consistency
+#caret_gbm_fit <- caret::train(as.factor(tcB) ~ . -tc -split -pagecount,
+ #                             data = df_train,
+  #                            method = 'gbm',
+   #                           distribution = 'bernoulli',
+    #                          trControl = fit_control,
+     #                         tuneGrid = gbmGrid,
                               # gbm passes this parameter
-                              verbose = FALSE)
+      #                        verbose = FALSE)
 
 # use caret_gbm_fit final values to create the optimal train gbm model
 optimal_gbm_train_model <- gbm(tcB ~ ., 
-                              data=select(df_train, -tc, -split, -pagecount),
-                              distribution = "bernoulli",
-                              n.trees = num_trees_var,
-                              shrinkage = .1,
-                          	  n.minobsinnode = 10,
-                              interaction.depth = 2,
-                              cv.folds = 10)
+                               data=select(df_train, -tc, -split, -pagecount),
+                               distribution = "bernoulli",
+                               n.trees = num_trees_var,
+                               shrinkage = 0.031,
+                               n.minobsinnode = 10,
+                               interaction.depth = 4,
+                               cv.folds = 10)
 
-print(caret_gbm_fit)
-plot(caret_gbm_fit)
+#print(caret_gbm_fit)
+#plot(caret_gbm_fit)
 
 print(optimal_gbm_train_model)
 
@@ -82,10 +90,10 @@ test_preds_with_gbm <- function(gbm_data, num_trees) {
   print(best_tree_for_prediction)
   summary(optimal_gbm_train_model)
   
-  cv.ifelse_preds <- ifelse(cv.gbm_predictions > mean(cv.gbm_predictions), 1, 0)
+  cv.ifelse_preds <- ifelse(cv.gbm_predictions > quantile(cv.gbm_predictions, c(0.75)), 1, 0)
   
   print(table(gbm_data$tcB))
-  print(table(cv.if_else_preds))
+  print(table(cv.ifelse_preds))
   
   roc_graph <- roc(gbm_data$tcB, cv.gbm_predictions,
                    ci=TRUE, ci.alpha=0.95, plot=TRUE,
@@ -94,9 +102,12 @@ test_preds_with_gbm <- function(gbm_data, num_trees) {
   sens.ci <- ci.se(roc_graph)
   plot(sens.ci, type="shape", col="lightblue")
   
-  auc_val <- auc(roc_graph)
+  auc_val <- pROC::auc(roc_graph)
   print(auc_val)
+  
+  precrec_obj <- precrec::evalmod(scores = cv.gbm_predictions, labels = gbm_data$tcB)
+  autoplot(precrec_obj) 
 }
 
-test_preds_with_gbm(df_dev, num_trees_var)
-#test_preds_with_gbm(df_test, num_trees_var)
+#test_preds_with_gbm(df_dev, num_trees_var)
+test_preds_with_gbm(df_test, num_trees_var)
